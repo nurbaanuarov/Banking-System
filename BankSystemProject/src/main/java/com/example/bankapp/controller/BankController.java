433391da79757ecd5e050c80cd8736ee71337ee0
nurbaanuarov <120207657+nurbaanuarov@users.noreply.github.com>
2025-04-22 14:18:10 +0500

    Add files via upload

---
+++ BankSystemProject/src/main/java/com/example/bankapp/controller/BankController.java
@@ -0,0 +1,131 @@
+package com.example.bankapp.controller;
+
+import com.example.bankapp.model.Account;
+import com.example.bankapp.model.UserRole;
+import com.example.bankapp.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Controller
+public class BankController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account = accountService.findAccountByUsername(username);
+        if (account.getRole().equals(UserRole.ADMIN)) {
+            model.addAttribute("users", accountService.getAllAccounts());
+            return "accounts";
+        }
+        model.addAttribute("account", account);
+        return "dashboard";
+    }
+
+    @PostMapping("/confirm-user")
+    public String confirmUser(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+
+        accountService.confirmUser(username);
+
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<String> createUser(@RequestParam String username, @RequestParam String password) {
+        if (username == null || password == null) {
+            return ResponseEntity.badRequest().body("Username and password are required");
+        }
+
+        try {
+            accountService.registerAccount(username, password);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User creation failed");
+        }
+
+        return ResponseEntity.ok("User created successfully");
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerAccount(@RequestParam String username, @RequestParam String password, Model model) {
+        try {
+            accountService.registerAccount(username, password);
+            return "redirect:/login";
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            return "register";
+        }
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/deposit")
+    public String deposit(@RequestParam BigDecimal amount) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account = accountService.findAccountByUsername(username);
+        accountService.deposit(account, amount);
+        return "redirect:/dashboard";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestParam BigDecimal amount, Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account = accountService.findAccountByUsername(username);
+
+        try {
+            accountService.withdraw(account, amount);
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("account", account);
+            return "dashboard";
+        }
+
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/transactions")
+    public String transactionHistory(Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account = accountService.findAccountByUsername(username);
+        model.addAttribute("transactions", accountService.getTransactionHistory(account));
+        return "transactions";
+    }
+
+    @PostMapping("/transfer")
+    public String transferAmount(@RequestParam String toUsername, @RequestParam BigDecimal amount, Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account fromAccount = accountService.findAccountByUsername(username);
+
+        try {
+            accountService.transferAmount(fromAccount, toUsername, amount);
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("account", fromAccount);
+            return "dashboard";
+        }
+
+        return "redirect:/dashboard";
+    }
+
+}
---
+++ BankSystemProject/src/main/java/com/example/bankapp/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.example.bankapp.model;
+
+public enum UserRole {
+    ADMIN,
+    CONFIRMED,
+    NOT_CONFIRMED
+}
